ce2e1dcba7cfbc6ffbd2a50fb6b720443f90ba53
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-22 19:55:54 +0100

    spelling mistake

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
 
 class Thing(models.Model):
-    name = models.CharFirld(unique = False, blank = False, max_length = 30)
-    description = models.CharFirld(unique = False, blank = False, max_length = 200)
+    name = models.CharField(unique = False, blank = False, max_length = 30)
+    description = models.CharField(unique = False, blank = False, max_length = 200)
     quantity = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(200)])
