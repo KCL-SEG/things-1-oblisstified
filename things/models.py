e41ddebfefa3522612828e02b5df8e038867650d
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-22 20:19:29 +0100

    changing values fails tests??

--- things/models.py
+++ things/models.py
@@ -3,6 +3,6 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.contrib.auth.models import AbstractUser
 
 class Thing(models.Model):
-    name = models.CharField(unique = True, blank = False, max_length = 30)
-    description = models.CharField(unique = False, blank = True, max_length = 120)
+    name = models.CharField(unique = False, blank = False, max_length = 30)
+    description = models.CharField(unique = False, blank = False, max_length = 120)
     quantity = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(100)])
