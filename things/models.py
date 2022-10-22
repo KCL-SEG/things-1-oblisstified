0b554a628141f477dc17181a076a6b740e8b3d7b
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-22 19:58:17 +0100

    fixed some validators

--- things/models.py
+++ things/models.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import AbstractUser
 
 class Thing(models.Model):
     name = models.CharField(unique = False, blank = False, max_length = 30)
-    description = models.CharField(unique = False, blank = False, max_length = 200)
-    quantity = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(200)])
+    description = models.CharField(unique = False, blank = True, max_length = 120)
+    quantity = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(100)])
