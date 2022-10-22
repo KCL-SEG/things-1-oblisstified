ee5b06c99ef41c9ddd78fa86c8f4ad40d15787a9
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-22 20:04:14 +0100

    fixed some constraints

--- things/models.py
+++ things/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class Thing(models.Model):
     name = models.CharField(unique = True, blank = False, max_length = 30)
     description = models.CharField(unique = False, blank = True, max_length = 120)
-    quantity = models.IntegerField(validators = [MinValueValidator(0),MaxValueValidator(100)],unique = False)
+    quantity = models.IntegerField(unique = False, validators = [MinValueValidator(0),MaxValueValidator(100)])
