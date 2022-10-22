52da39296f45c0b209b9fc0e82bb12b5b32fd1c5
aleksandr limanovskij <aleksandr.limanovskij@kcl.ac.uk>
2022-10-22 19:43:24 +0100

    add a basic things model

--- things/models.py
+++ things/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.db.models import Model,IntegerField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Thing(Model):
+    name = CharFirld(unique = False, blank = False, max_length = 30)
+    description = CharFirld(unique = False, blank = False, max_length = 200)
+    quantity = IntegerField(validators = [MinValueValidator(0),MaxValueValidator(100)])
